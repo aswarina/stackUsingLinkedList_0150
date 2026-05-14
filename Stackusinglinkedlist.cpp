4eb48eb0dc42dd8e2641f90ad7cb8c2bee697924
aswarina <lyxsyyawa@gmail.com>
2026-05-14 11:07:51 +0700

    Hubungin pilihan user ke fungsi stack

--- Stackusinglinkedlist.cpp
+++ Stackusinglinkedlist.cpp
@@ -93,3 +93,30 @@ int main()
         case 1:
             cout << "Enter the value to push: ";
             cin >> value;
+            stack.push(value);
+            break;
+
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty, cannot pop." << endl;
+            }
+            break;
+
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                cout << "Stack is empty. No top value." << endl;
+            }
+            break;
+
+        case 4:
+            cout << "Exiting program." << endl;
